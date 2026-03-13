2c7a60b478a775a017c4a3215e918ab01f260d6f
ELIAN-OSORIO <al19760601@ite.edu.mx>
2026-03-12 19:41:36 -0700

    Updates

--- main.js
+++ main.js
@@ -4,12 +4,12 @@ import Header from './components/header.js'
 import { apiData } from './helper/data.js';
 import './style.css'
 
-//const empresa = {
-  //titulo: 'Manejo de Sistemas Agricola',
-  //giro: 'Agricultura'
-//}
+const empresa = {
+  titulo: 'Manejo de Sistemas Agricola',
+  giro: 'Agricultura'
+}
 
-//document.body.appendChild(Header(empresa));
+document.body.appendChild(Header(empresa));
 const main = document.createElement('main');
 apiData.forEach((apiData) =>{
     main.appendChild(Card(apiData));
