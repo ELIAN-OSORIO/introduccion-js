bd805550414877a12c52894da4c00c2b5745a6d7
ELIAN-OSORIO <al19760601@ite.edu.mx>
2026-03-12 18:52:52 -0700

    Cards

--- introduccion/cards/components/Card.js
+++ introduccion/cards/components/Card.js
@@ -8,15 +8,27 @@ export default function Card({
   const card = document.createElement("div");
   card.className = "card";
   card.innerHTML = `
-    <img src="${profile_image}" alt="${description}" class='card-avatar'/>
-    <div class='card-info'>
-    <p class='card-name'>${employee_name}</p>
-    <p class='card-salary'>${employee_salary}</p>
-    <p class='card-age'>${employee_age}</p>
-    <p class='card-addres'>${address}</p>
-    <p class='card-number'>${phone_number}</p>
-    <p class='card-zipcode'>${zipcode}</p>
+    <img src="${profile_image}" alt="${description}" class="card-avatar"/>
+
+    <div class="card-info">
+      <!-- Nombre y Edad -->
+      <div class="card-row">
+        <span class="card-name">${employee_name}</span>
+        <span class="card-age">${employee_age}</span>
+      </div>
+
+      <!-- Salario y Dirección -->
+      <div class="card-row">
+        <span class="card-salary">${employee_salary}</span>
+        <span class="card-address">${address}</span>
+      </div>
+
+      <!-- Teléfono y Código Postal -->
+      <div class="card-row">
+        <span class="card-number">${phone_number}</span>
+        <span class="card-zip">${zipcode}</span>
+      </div>
     </div>
-    `;
+  `;
   return card;
 }
