de2744d9b4ce4b1f07875c991a8799160c36773c
ELIAN-OSORIO <al19760601@ite.edu.mx>
2026-03-12 21:14:21 -0700

    Changed Title

--- introduccion/cards/main.js
+++ introduccion/cards/main.js
@@ -4,12 +4,12 @@ import Header from './components/header.js'
 import { apiData } from './helper/data.js';
 import './style.css'
 
-// const empresa = {
-//   titulo: 'Manejo de Sistemas Agricola',
-//   giro: 'Agricultura'
-// }
+const empresa = {
+  titulo: 'Perfiles de Usuario',
+  giro: 'Rol de trabajo'
+}
 
-// document.body.appendChild(Header(empresa));
+document.body.appendChild(Header(empresa));
 const main = document.createElement('main');
 apiData.forEach((apiData) =>{
     main.appendChild(Card(apiData));
